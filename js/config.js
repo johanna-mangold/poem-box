368aa464d077446a839afb8ea13821d3acd3c150
jmangold <tausendwelt@gmail.com>
2025-12-27 17:02:30 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,29 @@
+(() => {
+  window.KMAP = window.KMAP || {};
 
+  window.KMAP.config = {
+    // CONNECTION LINES
+    CONNECTIONS: [
+      { id:"l1", pts: [ [0,0], [200,120], [420,-30] ] },
+      { id:"l2", pts: [ [420,-30], [680,120], [1220,40] ] },
+    ],
+    LINES: {
+      color: "rgba(255,255,255,0.80)",
+      width: 1.6,
+      dash:  [10, 10],
+      cap:   "round",
+      join:  "round",
+      dashScaleWithZoom: false
+    },
+
+    // IMAGES
+    MAP_IMAGES: [
+      // { src:"https://static.wixstatic.com/media/DEINBILD.png", x: 200, y: 140, w: 260, rot: 0, op: 1 },
+    ],
+
+    // YOUTUBE
+    MAP_YOUTUBE: [
+      {url:"https://youtu.be/9uCARmlj-KY", x: 800, y: 900, w: 480, h:270, rot:0, op: 1, mute: 1, controls: 1},
+    ]
+  };
+})();
