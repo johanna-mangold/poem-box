5db25351fa3eb5f82a7757daa106077e474237e7
jmangold <tausendwelt@gmail.com>
2025-12-29 14:34:41 +0100

    Update textBox.js
    text add

--- js/textBox.js
+++ js/textBox.js
@@ -61,6 +61,8 @@ General notes
 
 The map has no fixed beginning or end - explore freely.
 
+Or you can follow the blue-dotted-lines. Follow the blue-dotted-lines. Follow the blut-dotted-lines.
+
 You can always teleport back to the starting point by clicking on the white dot on the bottom of the screen.
       `.trim()
     }
