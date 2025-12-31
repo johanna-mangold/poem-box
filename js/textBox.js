a7fc795448566c73f6773cce63f94ec04e33d8e0
jmangold <tausendwelt@gmail.com>
2025-12-31 22:30:44 +0100

    Update textBox.js

--- js/textBox.js
+++ js/textBox.js
@@ -59,52 +59,52 @@ You can always teleport back to the starting point by clicking on the white dot 
       text: `
      reading 2025
      #### fiction
-- [[the heart is deceitful above above all things - jt leroy]] **
-- [[no one belongs here more than you - miranda july]] **
-- [[plastic abyss - kate wilhelm]] **
-- [[stranger in the house - kate wilhelm ]]**
-- [[the green millenium - fritz leiber]] **
-- [[where late the sweet birds sang - kate wilhelm]] **
-- [[margaret and i - kate willhelm]] **
-- [[the first bad man - miranda july]] **
-- [[mysterious skin - scott heim]] **
-- [[nightbitch - rachel yoder]] **
-- [[the eyes are the best part - monika kim]] **
-- [[the word for world is forest  - ursula le guin]] **
-- [[my year of rest and relaxation - ottessa moshfegh]] **
-- [[lügen über meine mutter - daniela dröscher]] **
-- [[david pablo - joanna yulia kluge]] **
-- [[für den rest des lebens - zeruya shalev]]
-- [[the dream hotel - laila lalami]] **
+[[the heart is deceitful above above all things - jt leroy]]
+[[no one belongs here more than you - miranda july]]
+[[plastic abyss - kate wilhelm]]
+[[stranger in the house - kate wilhelm ]]
+[[the green millenium - fritz leiber]] 
+[[where late the sweet birds sang - kate wilhelm]] 
+[[margaret and i - kate willhelm]] 
+[[the first bad man - miranda july]] 
+[[mysterious skin - scott heim]] 
+[[nightbitch - rachel yoder]] 
+[[the eyes are the best part - monika kim]] 
+[[the word for world is forest  - ursula le guin]] 
+[[my year of rest and relaxation - ottessa moshfegh]] 
+[[lügen über meine mutter - daniela dröscher]] 
+[[david pablo - joanna yulia kluge]] 
+[[für den rest des lebens - zeruya shalev]]
+[[the dream hotel - laila lalami]] 
 
 #### short stories
-- the yellow wallpaper - charlotte perkins gilman 
-- my evil mother - margaret atwood
-- bereue, harlekin! sagte der ticktackmann - harlan ellison
-- die stadt am rande der welt - harlan ellison
-- ich muss schreien und habe keinen mund - harlan ellison
-- two talented bastids (4*) - stephen king (you like it darker)
-- the fifth step (3*) - stephen king (you like it darker)
-- willie the weirdo (3*) - stephen king (you like it darker)
-- beyond lies the wub (2*) - philip k. dick (human is?) 
-- the defenders (5*) - philip k. dick (human is?) 
-- roog (2*) - philip k. dick (human is?) 
-- impostor (4*) - philip k. dick (human is?) 
-- the preserving machine (4*) - philip k. dick (human is?) 
-- the father-thing (3*) - philip k. dick (human is?) 
-- foster, you're dead (4*) - philip k. dick (human is?) 
-- human is? (5*) - philip k. dick (human is?) 
-- the infinity box (5*) - kate wilhelm (SF gateway omnibus)
-- the time piece (3*) - kate wilhelm (SF gateway omnibus)
+[[the yellow wallpaper - charlotte perkins gilman]]
+[[my evil mother - margaret atwood]]
+[[bereue, harlekin! sagte der ticktackmann - harlan ellison]]
+[[die stadt am rande der welt - harlan ellison]]
+[[ich muss schreien und habe keinen mund - harlan ellison]]
+[[two talented bastids - stephen king]]
+[[the fifth step - stephen king]]
+[[willie the weirdo - stephen king]]
+[[beyond lies the wub - philip k. dick]] 
+[[the defenders - philip k. dick]]
+[[roog - philip k. dick]]  
+[[impostor - philip k. dick]] 
+[[the preserving machine - philip k. dick]]  
+[[the father-thing - philip k. dick]] 
+[[foster, you're dead - philip k. dick]] 
+[[human is? - philip k. dick]] 
+[[the infinity box - kate wilhelm]] 
+[[the time piece - kate wilhelm]] 
 
 #### non-fiction
-- [[hexen - die unbesiegte macht der frauen - mona chollet]] **
-- [[kulturelle aneignung - lars distelhorst (nautilus)]] **
-- [[six ways - aidan wachter]] **
-- [[we need your art - amy mcnee]] **
-- [[künstliche intelligenz und der neue faschismus - rainer mühlhoff (reclam)]] **
-- [[künstliche intelligenz und empathie - catrin misselhorn]] **
-- [[das seltsame und das gespenstische - mark fisher]] **
+[[hexen - die unbesiegte macht der frauen - mona chollet]]
+[[kulturelle aneignung - lars distelhorst (nautilus)]]
+[[six ways - aidan wachter]]
+[[we need your art - amy mcnee]]
+[[künstliche intelligenz und der neue faschismus - rainer mühlhoff (reclam)]]
+[[künstliche intelligenz und empathie - catrin misselhorn]]
+[[das seltsame und das gespenstische - mark fisher]]
      `.trim()
       }
   ];
