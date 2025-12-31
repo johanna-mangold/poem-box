f610e6ae2d85d7b8b878e258f5b72d98085cd715
jmangold <tausendwelt@gmail.com>
2025-12-31 17:07:41 +0100

    Update textBox.js

--- js/textBox.js
+++ js/textBox.js
@@ -76,6 +76,36 @@ You can always teleport back to the starting point by clicking on the white dot 
 - [[david pablo - joanna yulia kluge]] **
 - [[für den rest des lebens - zeruya shalev]]
 - [[the dream hotel - laila lalami]] **
+
+#### short stories
+- the yellow wallpaper - charlotte perkins gilman 
+- my evil mother - margaret atwood
+- bereue, harlekin! sagte der ticktackmann - harlan ellison
+- die stadt am rande der welt - harlan ellison
+- ich muss schreien und habe keinen mund - harlan ellison
+- two talented bastids (4*) - stephen king (you like it darker)
+- the fifth step (3*) - stephen king (you like it darker)
+- willie the weirdo (3*) - stephen king (you like it darker)
+- beyond lies the wub (2*) - philip k. dick (human is?) 
+- the defenders (5*) - philip k. dick (human is?) 
+- roog (2*) - philip k. dick (human is?) 
+- impostor (4*) - philip k. dick (human is?) 
+- the preserving machine (4*) - philip k. dick (human is?) 
+- the father-thing (3*) - philip k. dick (human is?) 
+- foster, you're dead (4*) - philip k. dick (human is?) 
+- human is? (5*) - philip k. dick (human is?) 
+- the infinity box (5*) - kate wilhelm (SF gateway omnibus)
+- the time piece (3*) - kate wilhelm (SF gateway omnibus)
+
+#### non-fiction
+- [[hexen - die unbesiegte macht der frauen - mona chollet]] **
+- [[kulturelle aneignung - lars distelhorst (nautilus)]] **
+- [[six ways - aidan wachter]] **
+- [[we need your art - amy mcnee]] **
+- [[künstliche intelligenz und der neue faschismus - rainer mühlhoff (reclam)]] **
+- [[künstliche intelligenz und empathie - catrin misselhorn]] **
+- [[das seltsame und das gespenstische - mark fisher]] **
+- [[effektive mikroorganismen und ihre praktische anwendung - dr. anne katharina zschocke]] **
      `.trim()
       }
   ];
